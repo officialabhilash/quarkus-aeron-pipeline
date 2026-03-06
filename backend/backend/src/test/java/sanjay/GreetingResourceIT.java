9d36e47126a8bf78c5d4cd13ede70b3fdbca80e3
Abhilash <theak.92@gmail.com>
2026-03-07 02:15:40 +0530

    FEATURE: Added initial application code with name

---
+++ backend/backend/src/test/java/sanjay/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package sanjay;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
